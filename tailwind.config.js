fbd70026a43f99a8685e3e1218d3599840f25a50
Nico Prat <nicooprat@gmail.com>
2020-11-20 10:00:30 +0100

    Scaffolding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
   purge: [],
   theme: {
     extend: {},
+    colors: {
+      primary: '#00ce41',
+    },
+    fontFamily: {
+      sans: ['Roboto', 'system-ui', '-apple-system', 'Segoe UI', 'Roboto', 'Ubuntu', 'Cantarell', 'Noto Sans', 'sans-serif', 'BlinkMacSystemFont', '-apple-system', 'Segoe UI', 'sans-serif'],
+    }
   },
   variants: {},
   plugins: [],
